4addcaadf2200896bac70397e500eff468f213f1
arlettearenas <arlettearenas29@gmail.com>
2024-02-25 00:12:32 -0600

    08 React Server Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+
 import styles from "./page.module.css";
 
 export default function Home() {
+  console.log('Hola mundo pagina de inicio')
   return (
     <main className={styles.main}>
       <h1>Hola mundo</h1>
