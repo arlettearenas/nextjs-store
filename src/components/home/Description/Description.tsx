994c2160254607142741e6bc11657298e2efc2ef
arlettearenas <arlettearenas29@gmail.com>
2024-02-29 20:03:14 -0600

    14 Manejo y optimización de imagenes

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,9 +1,26 @@
+import Image from 'next/image'
+import styles from './Description.module.sass'
+
+
 export const Description = () => {
+
     return (
-        <section>
-            <img src="/images/description.jpeg" alt="products marketplace" />
-            <h2>Description</h2>
+        <section className={styles.Description}>
+            <Image
+
+            src="/images/description.jpeg"
+
+            alt="products marketplace"
+
+            width={500}
+
+            height={300}
+            />
+
+            <div className={styles.Description__text}>
+            <h2>Bring the future today</h2>
             <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga officiis modi repellat obcaecati, reprehenderit delectus vitae. Vel assumenda dolore amet magnam officiis alias consequuntur eveniet, magni adipisci totam exercitationem aut!</p>
+            </div>
         </section>
     )
 }
