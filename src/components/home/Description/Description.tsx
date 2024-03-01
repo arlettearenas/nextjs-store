040e753ba48584387ce2042306a6b485cc51aac7
arlettearenas <arlettearenas29@gmail.com>
2024-02-29 19:42:29 -0600

    13 Como agregar archivos estaticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,9 @@
 export const Description = () => {
     return (
         <section>
-            <h1>Descripción</h1>
+            <img src="/images/description.jpeg" alt="products marketplace" />
+            <h2>Description</h2>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga officiis modi repellat obcaecati, reprehenderit delectus vitae. Vel assumenda dolore amet magnam officiis alias consequuntur eveniet, magni adipisci totam exercitationem aut!</p>
         </section>
     )
 }
