14b50e2d8b572922151f65a42b344d9dd2df2e5a
Postdie <fernandezdiego845@gmail.com>
2024-03-11 16:59:27 -0600

    Commit número 18

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 export const MainProducts = () => {
+    console.log("variable de entorno", process.env.NEXT_PUBLIC_SHOPIFY_HOSTMANE)
     return (
         <section>
             <h1>MainProducts</h1>
