110668f65489478a3e88d986f25cbaa0ccf55dcf
arlettearenas <arlettearenas29@gmail.com>
2024-03-22 10:29:01 -0600

    28 Patrones de data fetching

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,21 +1,25 @@
 import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
-import { getCollections } from "app/services/shopify/collections";
-import { getProducts } from "app/services/shopify/products";
+import { getCollectionProducts, getCollections } from "app/services/shopify/collections"
+import { getProducts } from "app/services/shopify/products"
 
-interface  CategoryProps {
-    params:{
+interface CategoryProps {
+    params: {
         categories: string[],
-        searchParams?: string
     }
-}
-export default async function Category(props:CategoryProps){
-const products = await getProducts()
+    searchParams?: string
+    }
+    export default async function Category(props: CategoryProps) {
+        const { categories } = props.params
+        let products = []
+        const collections = await getCollections()
+        if (categories?.length > 0) {
+            const selectedCollectionId = collections.find((collection: any) => collection.handle === categories[0]).id
+            products = await getCollectionProducts(selectedCollectionId)
+        }else {
+            products = await getProducts()
+        }
 
-const collections= await getCollections()
-console.log(collections)
-    const {categories}=props.params;
-    //throw new Error('Error: Boom!')
-    return(
-        <ProductsWrapper products={products}/>
-    )
-}
+        return (
+            <ProductsWrapper products={products} />
+        )
+    }
