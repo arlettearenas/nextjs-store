8ed5d308ec241bd4a6e5477551b197cf7a30c6da
arlettearenas <arlettearenas29@gmail.com>
2024-03-22 10:19:09 -0600

    28 Patrones de data fetching

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,19 +1,21 @@
 import { ProductsWrapper } from "app/components/Store/ProductsWrapper"
-import { getProducts } from "app/services/shopify/products"
+import { getCollections } from "app/services/shopify/collections";
+import { getProducts } from "app/services/shopify/products";
 
-interface CategoriesProps{
-    params: {
-        categories: string[]
-        searchParams: string
+interface  CategoryProps {
+    params:{
+        categories: string[],
+        searchParams?: string
     }
 }
+export default async function Category(props:CategoryProps){
+const products = await getProducts()
 
-export default async function Categories(props: CategoriesProps){
-
-    const products = await getProducts()
-    const { categories } = props.params
-    // throw new Error('Error:Boom!'
+const collections= await getCollections()
+console.log(collections)
+    const {categories}=props.params;
+    //throw new Error('Error: Boom!')
     return(
         <ProductsWrapper products={products}/>
-        );
+    )
 }
