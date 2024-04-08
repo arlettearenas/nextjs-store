8250314556bdd8608603cfc5ceb75410de491da2
arlettearenas <arlettearenas29@gmail.com>
2024-04-07 22:12:00 -0600

    34 Mejorando un SEO de una pagina

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,15 @@
-import { MainProducts } from "app/components/home/MainProducts";
+import { MainProducts } from "app/components/home/MainProducts"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "✨ Future world",
+  description: "Welcome to the future world, an ecommerce from other century",
+  keywords: ["ecommerce", "future", "world","technology"],
+}
 
 export default function Home() {
   return (
     <main>
-      <h1>product</h1>
-      {/* <h1>product</h1> */}
-
-      {/* <MainProducts/> */}
-      <MainProducts/> 
-
+      <MainProducts />
     </main>
-  );
-}
+  )}
--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,16 +1,35 @@
-import { useParams, useSearchParams } from "next/navigation"
-
+import { ProductView } from "app/app/product/ProductView"
+import { getProducts } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
 
 interface ProductPageProps {
     searchParams: {
-    id: string
-}
+      id: string
+    }
+  }
+  export async function generateMetadata({ searchParams }: ProductPageProps) { 
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  return {
+    title: product.title,
+    description: product.description,
+    keywords: product.tags,
+    openGraph: {
+      images: [product.image]
+    }
+  }
 }
 
-export default function ProductPage() {
-    const params = useParams()
-    const searchParams= useSearchParams()
-    const id=searchParams.get("id")
-    console.log(searchParams,id)
-    return <h1>Product Page</h1>
+export default async function ProductPage({ searchParams }: ProductPageProps) {
+  const id = searchParams.id
+  const products = await getProducts(id)
+  const product = products[0]
+
+  if (!id) {
+    redirect('/')
+  }
+
+  return <ProductView product={product} />
 }
