eb49cb153b2a4b463b671d934743d3eea3e701f5
arlettearenas <arlettearenas29@gmail.com>
2024-04-20 01:17:01 -0600

    39 Validacion de token de acceso de usuario

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,21 +1,26 @@
-import Link from 'next/link';
-export const Header = () => {
-    console.log('Hola mundo header')
-    return (
-        <header>
-        <nav>
-            <ul>
-            <Link href="/">
-            <li>Home</li>
-            </Link>
-            <Link href="/store">
+import Link from "next/link"
+import styles from './Header.module.css'
+import { validateAccessToken } from "app/utils/auth/validateAccesToken"
+
+
+export const Header = async () =>{
+  const customer = await validateAccessToken()
+  return (
+    <header>
+      <nav>
+        <ul>
+        <Link href="/" className="link">
+          <li>Home</li>
+          </Link>
+          <Link href="/store" className="link">
             <li>Store</li>
             </Link>
-            <Link href="/test">
+            <Link href="/test" className="link">
             <li>Test</li>
             </Link>
-            </ul>
-        </nav>
+        </ul>
+        {customer?.firsName ? (<p>Hola!{customer.firsName}</p>) : (<Link href="/login"></Link>)}
+          </nav>
         </header>
     )
 }
